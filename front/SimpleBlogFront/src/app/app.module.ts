ad806624f7b3b899be465e2696de8149a6ded435
Alisboa13 <Alisboa13@alumnos.utalca.cl>
2018-12-03 19:52:47 -0300

    scaffolding and journal

--- front/SimpleBlogFront/src/app/app.module.ts
+++ front/SimpleBlogFront/src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { RecoverPageComponent } from './components/recover-page/recover-page.component';
+import { FrontPageComponent } from './components/front-page/front-page.component';
+import { BlogFormPageComponent } from './components/blog-form-page/blog-form-page.component';
+import { BlogPageComponent } from './components/blog-page/blog-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
+    RecoverPageComponent,
+    FrontPageComponent,
+    BlogFormPageComponent,
+    BlogPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ front/SimpleBlogFront/src/app/components/blog-form-page/blog-form-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-form-page',
+  templateUrl: './blog-form-page.component.html',
+  styleUrls: ['./blog-form-page.component.css']
+})
+export class BlogFormPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
