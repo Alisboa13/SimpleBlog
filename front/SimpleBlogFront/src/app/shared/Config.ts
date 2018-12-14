0ea5521a65beb2d30106b0b8b5962b315a3e773f
Alisboa13 <Alisboa13@alumnos.utalca.cl>
2018-12-13 22:03:07 -0300

    changed default endpoints

--- front/SimpleBlogFront/src/app/shared/Config.ts
+++ front/SimpleBlogFront/src/app/shared/Config.ts
@@ -1,6 +1,6 @@
 export class Config{
-    public static BLOG_API = "http://10.0.0.2:3000/api/blogs/"
-    public static COMMENT_API = "http://10.0.0.2:3000/api/comments/"
-    public static AUTHORIZATION_API= 'http://10.0.0.2:3000/api/bunnies/login';
-    public static USER_API = "http://10.0.0.2:3000/api/bunnies/"
+    public static BLOG_API = "/api/blogs/"
+    public static COMMENT_API = "/api/comments/"
+    public static AUTHORIZATION_API= '/api/bunnies/login';
+    public static USER_API = "/api/bunnies/"
 }
